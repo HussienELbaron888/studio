8889b373549e1a20167683648508840157b15a32
Hussien ELbaron <hussienelbaron888@gmail.com>
2025-09-30 10:31:10 +0000

    تغيرت الرسالة الان

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-
 // WARNING: This is not a secure practice for production.
 // We are placing the API key here temporarily for debugging purposes.
+// Please replace 'YOUR_BREVO_API_KEY' with your actual key.
 export const brevoApiKey = 'YOUR_BREVO_API_KEY';
