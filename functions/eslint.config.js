a6442bb487cde98898b244c0c5c835f4f871bd92
Hussien ELbaron <hussienelbaron888@gmail.com>
2025-10-04 14:08:09 +0000

    Error: functions predeploy error: Command terminated with non-zero exit

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -1,10 +1,9 @@
-
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import globals from 'globals';
-import eslintPluginImport from 'eslint-plugin-import';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import eslintPluginImport from "eslint-plugin-import";
 
 export default tseslint.config(
   {
@@ -19,20 +18,19 @@ export default tseslint.config(
         ...globals.node,
       },
       parserOptions: {
-        project: ["tsconfig.json", "tsconfig.dev.json"],
-        tsconfigRootDir: import.meta.dirname,
+        project: true,
       },
     },
     plugins: {
       import: eslintPluginImport,
     },
     rules: {
-        "quotes": ["error", "double"],
-        "import/no-unresolved": "off", // Turned off as it can be problematic in monorepos
-        "indent": ["error", 2],
-        "no-unused-vars": "warn",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "@typescript-eslint/no-explicit-any": "warn",
+      "quotes": ["error", "double"],
+      "import/no-unresolved": "off", // Turned off as it can be problematic in monorepos
+      "indent": ["error", 2],
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 );
